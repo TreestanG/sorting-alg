f39cf78f581364ae90344ca74c991d8d30611e2f
Koolwiza <108306482+TreestanG@users.noreply.github.com>
2023-06-17 12:28:02 +0800

    Added quicksort

--- index.py
+++ index.py
@@ -5,12 +5,9 @@ pygame.init()
 pygame.display.set_caption('Sorting Algorithm')
 
 width, height, tower_width = 600, 600, 2
+sorting, finished, si, fi, sort_type = False, False, 0, 0, "bubble"
 screen = pygame.display.set_mode((width, height))
 towers = random.sample([(x+1)*tower_width for x in range(math.floor(width/tower_width))], math.floor(width/tower_width))
-sorting = False
-finished = False
-si = 0
-fi = 0
 
 while True:
     for event in pygame.event.get():
@@ -18,27 +15,41 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
-            if event.key == K_DOWN:
-                sorting = True
             if event.key == K_UP:
                 towers = random.sample([(x+1)*tower_width for x in range(math.floor(width/tower_width))], math.floor(width/tower_width))
                 sorting = False
                 finished = False
                 si = 0
                 fi = 0
+            else:
+                sorting = True
+                if event.key == K_q:
+                    sort_type = "quick"
+                if event.key == K_b:
+                    sort_type == "bubble"
     
     if sorting:
-        
-        if towers == sorted(towers, reverse=True):
+        if towers == sorted(towers):
             sorting = False
             finished = True
 
-        if si < len(towers):
-            if max(towers) != towers[si]:
-                gr_ind = towers.index(max(towers[si:]))
-                towers[si], towers[gr_ind] = towers[gr_ind], towers[si]
+        if sort_type == "bubble":
+            if si < len(towers):
+                if min(towers) != towers[si]:
+                    gr_ind = towers.index(min(towers[si:]))
+                    towers[si], towers[gr_ind] = towers[gr_ind], towers[si]
+                    si += 1
+        elif sort_type == "quick":
+            
+            if si < len(towers)-1:
+                next_num = towers[si+1]
+                if towers[si] > next_num:
+                    towers.pop(si+1)
+                    for i in towers:
+                        if i > next_num:
+                            towers.insert(towers.index(i), next_num)
+                            break
                 si += 1
-
 
     if finished: 
         pygame.draw.rect(screen, (0,255,0), (fi*2, height-fi*2, 2, fi*2))
@@ -47,7 +58,7 @@ while True:
     else:
         screen.fill((255,255,255))
         for i in range(len(towers)):
-            pygame.draw.rect(screen, (0,0,0), (i*tower_width, 0, tower_width, towers[i]))
+            pygame.draw.rect(screen, (0,0,0), (i*tower_width, 0, tower_width, height-towers[i]))
         
 
     pygame.display.flip()
